a17a64b7ca3eafa55e3a9565a27b2ba1a31dd78e
hanlee <me@hanlee.co>
2021-06-09 19:28:49 +0900

    feat: add 404 page

---
+++ src/pages/[...all].vue
@@ -0,0 +1,10 @@
+<template>
+  <main>
+    <h1 class="text-3xl font-bold py-16">
+      404 Not Found
+    </h1>
+    <router-link to="/" class="hover:text-secondary">
+      返回首頁
+    </router-link>
+  </main>
+</template>
