3dadeada9e278753092e647999476c3cdd1744e9
Josh <a777441a@gmail.com>
2021-06-11 21:29:50 +0800

    add: popover menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,14 +6,37 @@ defineProps({})
 
 const { isAuthorized } = useAuth()
 
+const menuList = [
+  {
+    title: '一線單位需求',
+    path: '',
+  },
+  {
+    title: '愛心捐贈',
+    path: '',
+  },
+  {
+    title: '相關平台',
+    path: '',
+  },
+]
+
 </script>
 
 <template>
   <header class="fixed top-0 inset-x-0 border-b bg-white px-2">
     <div class="container max-w-screen-xl mx-auto h-$header-height flex items-center justify-between text-primary">
-      <button class="icon-btn">
+      
+      <popover class="icon-btn">
         <ic:round-menu />
-      </button>
+        <template #content>
+          <ul class="rounded border bg-white -mt-2 ml-2 py-1 text-base text-black">
+            <li v-for="(menu, index) in menuList" :key="index" class="py-2.5 px-6 active:text-primary">
+              {{ menu.title}}
+            </li>
+          </ul>
+        </template>
+      </popover>
       <router-link to="/" class="block">
         <img src="/img/logo.png" alt="Shared TW" class="h-55px">
       </router-link>
